52ca87fe72f41b40cc75da4eeb658e56203a88a2
Dobber Peas <dobberpe@student.21-school.ru>
2024-08-20 13:34:11 +0300

    load ok

--- src/main_window/main_window.h
+++ src/main_window/main_window.h
@@ -10,19 +10,48 @@
 
 #include "graphics/graphics.h"
 
+#define ROTATE true
+#define MOVE false
+
 class main_window : public QMainWindow {
   Q_OBJECT
-  friend class Viewer;
-
  public:
   explicit main_window(QWidget *parent = nullptr);
+
+ private slots:
+  void on_loadButton_clicked();
+  void on_rotationXSlider_valueChanged(int value);
+  void on_rotationYSlider_valueChanged(int value);
+  void on_rotationZSlider_valueChanged(int value);
+  void on_moveXSlider_valueChanged(int value);
+  void on_moveYSlider_valueChanged(int value);
+  void on_moveZSlider_valueChanged(int value);
+  void on_scaleSlider_valueChanged(int value);
+  void on_increaseScaleButton_clicked();
+  void on_decreaseScaleButton_clicked();
+  void on_screenshotButton_clicked();
 
  private:
   void rotate_slider(double rotate_X, double rotate_Y, double rotate_Z);
   void move_slider(double move_X, double move_Y, double move_Z);
   void scale_slider(double scale);
+  void setupUI();
+  void setupSliderBox(QVBoxLayout *rightColumnLayout, bool rotate);
 
   Viewer *v;
+
+  QPushButton *loadButton;
+  QSlider *rotationXSlider;
+  QSlider *rotationYSlider;
+  QSlider *rotationZSlider;
+  QSlider *moveXSlider;
+  QSlider *moveYSlider;
+  QSlider *moveZSlider;
+  QSlider *scaleSlider;
+  QPushButton *increaseScaleButton;
+  QPushButton *decreaseScaleButton;
+  QPushButton *screenshotButton;
+
   int curr_moveX = 0;
   int curr_moveY = 0;
   int curr_moveZ = 0;
